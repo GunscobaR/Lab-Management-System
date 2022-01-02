e96711dfbe31944c6b9e0907df3da045943504d2
GunscobaR <96984662+GunscobaR@users.noreply.github.com>
2022-01-02 22:08:38 +0800

    Add files via upload

---
+++ database/lab_db.sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 14, 2021 at 04:50 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checked`
+--
+
+CREATE TABLE `checked` (
+  `id` int(30) NOT NULL,
+  `ref_no` varchar(100) NOT NULL,
+  `room_id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `contact_no` varchar(20) NOT NULL,
+  `date_in` datetime NOT NULL,
+  `date_out` datetime NOT NULL,
+  `booked_cid` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = pending, 1=checked in , 2 = checked out',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `checked`
+--
+
+INSERT INTO `checked` (`id`, `ref_no`, `room_id`, `name`, `contact_no`, `date_in`, `date_out`, `booked_cid`, `status`, `date_updated`) VALUES
+(12, '9484225051\n', 3, 'MOHD BAHRIN BIN ABDI RAHMAN', '', '2021-11-14 15:03:00', '2021-11-15 15:03:00', 1, 2, '2021-11-14 22:43:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `id` int(30) NOT NULL,
+  `room` varchar(30) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = Available , 1= Unvailables'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rooms`
+--
+
+INSERT INTO `rooms` (`id`, `room`, `category_id`, `status`) VALUES
+(1, 'Room-101', 3, 0),
+(3, 'Room-102', 2, 0),
+(5, 'Room-103', 6, 0),
+(6, 'Room-104', 4, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room_categories`
+--
+
+CREATE TABLE `room_categories` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `price` float NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room_categories`
+--
+
+INSERT INTO `room_categories` (`id`, `name`, `price`, `cover_img`) VALUES
+(2, 'Deluxe Lab', 500, '2.jpg'),
+(3, 'Single Lab', 120, '1.jpg'),
+(4, 'Super Size Lab', 350, '4.jpg'),
+(6, 'Premium Lab', 200, '3.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `lab_name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `lab_name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'Lab Management System', 'info@sample.com', '+6948 8542 623', 'cover.jpg', 'About\r\nITS maintains and manages the Central Teaching Computer Labs Located at Building 9 Level 4, Building 9 Level 5 and Building 6 Level 6. ITS operates 7 fully equipped computer laboratories with a total of 249 computers. The Central Teaching Computer Labs are fully furnished to provide a conducive environment for lab classes, research, assignments, and projects. The Central Teaching Computer Labs are equipped with excellent audio/visual aids. Besides the computer laboratories, we also manage 115 desktop computers and 49 notebooks that are located in Building 7 Level 1 and Level 3, Library and Learning Commons, and 6 desktop computers at Building 9 Level 3, ITS Service Desk, exclusively for Internet browsing, assignments, printing and projects.\r\n\r\nThere are an additional 5 computer lab called the Next Gen Labs located at Building 6B Level 6. One of these labs (Next Gen Lab 4) also has specialized VC equipment for inter-campus teaching. These equipment will require specific training for use. ITS also recommends trial sessions to be conducted in advance if teaching is planned in that facility to familiarize with the equipment functionality.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=admin , 2 = staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', 'admin123', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `checked`
+--
+ALTER TABLE `checked`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room_categories`
+--
+ALTER TABLE `room_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `checked`
+--
+ALTER TABLE `checked`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `room_categories`
+--
+ALTER TABLE `room_categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
